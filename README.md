84ef9d15a0f257e93c2e2a208402cf2acb88011b
KDAVID9h <hognonkossidavid@gmail.com>
2024-09-04 00:22:48 +0000

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# HabitTracker
+
+## Description
+HabitTracker is a web application designed to help users track their daily habits and routines. Users can add habits, mark them as completed for each day, and view statistics on their habit completion.
+
+## Features
+- User registration and authentication
+- Add and manage habits
+- Mark habits as completed
+- View habits in a dashboard
+- Notifications and reminders (coming soon)
+
+## Technologies
+- Backend: React, firebase
+- Frontend: HTML, CSS, JavaScript
+
+## Setup and Installation
+1. Clone the repository
+   ```bash
+   git clone https://github.com/KDAVID9h/HabitTracker-Portfolio-Project
---
+++ src/app/unathenticated-app/layout.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { alpha, Box, hexToRgb, useTheme } from '@material-ui/core';
+import bg_hkde from 'images/bg_hkde.png';
+
+/**
+ * Layout with background image
+ */
+function Layout({ children }) {
+  return (
+    <BackgroundImage>
+      <Box
+        sx={{
+          minHeight: '100vh',
+          maxWidth: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        {children}
+      </Box>
+    </BackgroundImage>
+  );
+}
+
+/**
+ * Clones child component and adds a background image styles.
+ */
+function BackgroundImage({ children }) {
+  const { light, dark } = useTheme().palette.primary;
+
+  const lightRgb = hexToRgb(light);
+  const darkRgb = hexToRgb(dark);
+
+  return (
+    <Box
+      clone
+      sx={{
+        background: `
+          linear-gradient(
+            to right bottom, 
+            ${alpha(lightRgb, 0.3)}, 
+            ${alpha(darkRgb, 0.8)}), 
+          url(${bg_hkde})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+export { Layout };
